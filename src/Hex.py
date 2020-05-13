d932ac814e424d2d67ab05b8da86076446867faa
Riesi <stefan.riesenberger@gmail.com>
2020-05-13 13:45:07 +0200

    update copyright in files

--- src/Hex.py
+++ src/Hex.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 """
-Copyright (C) 2012-2017 Kirill Kulakov, Jose Carlos Granja & Xerxes Ranby
+Copyright (C) 2012-2020 Kirill Kulakov, Jose Carlos Granja, Xerxes Ranby & Stefan Riesenberger
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
